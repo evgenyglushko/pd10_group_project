83bd77b71b3360c7b487f3d72b40286c5ad01f7d
mkN <mknord1565@gmail.com>
2022-06-14 00:06:05 +0300

    Added class BasicWord

--- basic_word.py
+++ basic_word.py
@@ -1,2 +1,23 @@
 class BasicWord:
-    pass
+
+    def __init__(self, word: str, subwords: list):
+        self.word = word
+        self.subwords = subwords
+
+    def __repr__(self):
+        return f"'{self.word}'.BasicWord"
+
+    def check_subword_in_subwords(self, subword: str) -> bool:
+        """
+        Проверяет наличие подслова в списке подслов
+        :param subword:
+        :return: bool
+        """
+        return subword in self.subwords
+
+    def get_subwords_quantity(self) -> int:
+        """
+        Возвращает количество подслов
+        :return: int
+        """
+        return len(self.subwords)
