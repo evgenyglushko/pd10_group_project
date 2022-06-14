f2ec13e6e72159c10fa69769e4579ab6ce4cacca
mkN <mknord1565@gmail.com>
2022-06-14 21:36:10 +0300

    1. В классе BasicWord добавлено приведение поля subwords к типу set при инициализации. 2. В классе BasicWord изменено название метода проверки наличия подслова в списке подслов. Теперь - is_subword(). 3. В классе BasicWord - правки в докстринге.

--- basic_word.py
+++ basic_word.py
@@ -2,22 +2,22 @@ class BasicWord:
 
     def __init__(self, word: str, subwords: list):
         self.word = word
-        self.subwords = subwords
+        self.subwords = set(subwords)
 
     def __repr__(self):
         return f"'{self.word}'.BasicWord"
 
-    def check_subword_in_subwords(self, subword: str) -> bool:
+    def is_subword(self, subword: str) -> bool:
         """
         Проверяет наличие подслова в списке подслов
-        :param subword:
-        :return: bool
+        :param str subword:
+        :return bool: True если найдено, False если нет
         """
         return subword in self.subwords
 
     def get_subwords_quantity(self) -> int:
         """
         Возвращает количество подслов
-        :return: int
+        :return int:
         """
         return len(self.subwords)
